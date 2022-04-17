a2ed753792ab1b69d2d060c6ac0f3834f7b3d021
Jessica <jess.newman19@gmail.com>
2022-04-17 17:53:40 -0400

    commit tree 0f6cb856c99264f213a582390fbc0ad61fd32ff7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+function submitData (name, email) {
+  const data = { 
+    name: "Steve", 
+    email: "steve@steve.com"
+  }
+  return fetch ('http://localhost:3000/users', { 
+    method: "POST", 
+    headers: { 
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    }, 
+    body: JSON.stringify(data)
+  })
+  .then (response => response.json())
+  .then (userData => { 
+    const id = document.createElement("h1")
+    document.querySelector("body").appendChild(id)
+    id.innerText = userData.id
+  })
+  .catch((error) => { 
+    alert(error.message)
+  })
+}
+  
+  submitData()
